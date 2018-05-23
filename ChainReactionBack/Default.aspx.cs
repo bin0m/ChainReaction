8fd344c66ef46c3dc25c0618d83475c2f207e4ae
bin0m <levitg@gmail.com>
2018-05-23 16:39:12 +0300

    R set up

--- ChainReactionBack/Default.aspx.cs
+++ ChainReactionBack/Default.aspx.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ChainReactionBack.Models;
+using RDotNet;
 
 namespace ChainReactionBack
 {
@@ -22,8 +23,132 @@ namespace ChainReactionBack
         protected void Page_Load(object sender, EventArgs e)
         {
             CurrencyLoad();
-            SeedDatabase();
+            ReccomendProducts();
         }
+
+        private void ReccomendProducts()
+        {
+            //тут получаем тип пользователя
+            var usertype = "guest";
+
+            //StartupParameter rinit = new StartupParameter();
+            //rinit.Quiet = true;
+            //rinit.RHome = "C:/Program Files/R/R-3.5.0";
+            //rinit.Interactive = true;
+            REngine.SetEnvironmentVariables();
+           // REngine engine = REngine.GetInstance(null, true, rinit);
+            REngine engine = REngine.GetInstance();
+            engine.Evaluate("require(tidyr)");
+            #region Подключаем пакеты R, если их нет, то устанавливаем и подключаем
+            try {  }
+            catch (EvaluationException e)
+            {
+                //engine.Evaluate("install.packages('tidyr')");
+                engine.Evaluate("library(tidyr)");
+            }
+            try { engine.Evaluate("library(recommenderlab)"); }
+            catch (EvaluationException e)
+            {
+                engine.Evaluate("install.packages(recommenderlab)");
+                engine.Evaluate("library(recommenderlab)");
+            }
+            try { engine.Evaluate("library(dplyr)"); }
+            catch (EvaluationException e)
+            {
+                engine.Evaluate("install.packages(dplyr)");
+                engine.Evaluate("library(dplyr)");
+            }
+
+            try { engine.Evaluate("library(RODBC)"); }
+            catch (EvaluationException e)
+            {
+                engine.Evaluate("install.packages('RODBC')");
+                engine.Evaluate("library(RODBC)");
+            }
+            engine.Evaluate("library(RODBC)");
+            #endregion
+
+            engine.Evaluate("connectionString <- 'Driver ={ ODBC Driver 13 for SQL Server}; Server = tcp:chainreaction.database.windows.net,1433; Database = chainreaction; Uid = hacker@chainreaction; Pwd = kotbumnahpro4$; Encrypt = yes; TrustServerCertificate = no; Connection Timeout = 30;'");
+
+
+            engine.Evaluate("myconn < -odbcDriverConnect(connectionString))");
+
+            if (usertype == "guest")
+            {
+                // взять из бд
+                var userid_fromhtml = "34";
+                //engine.Evaluate("users < -sqlQuery(myconn, 'SELECT * FROM users'");
+                //engine.Evaluate("transactions < -sqlQuery(myconn, 'SELECT * FROM transactions')");
+                //engine.Evaluate(" products < -sqlQuery(myconn, 'SELECT * FROM products')");
+                engine.Evaluate("users < -read.csv('C:\\git\\ChainReaction\\users.csv', sep=';')");
+                engine.Evaluate("transactions < -read.csv('C:\\git\\ChainReaction\\transactions.csv', sep=';')");
+                engine.Evaluate("products < -read.csv('C:\\git\\ChainReaction\\products.csv', sep=';')");
+                engine.Evaluate("products$description < - as.character(products$description)");
+                engine.Evaluate("products$name < - as.character(products$name)");
+                engine.Evaluate(" products < -dplyr::rename(products, 'product' = 'id')");
+                engine.Evaluate("userid_fromhtml=" + userid_fromhtml);
+                engine.Evaluate("walletCertain = users[which(users$id == userid_fromhtml),]");
+                engine.Evaluate("walletCertain = walletCertain$wallet");
+                engine.Evaluate("trCertainUser = dplyr::filter(transactions, as.character(from) == as.character(walletCertain))");
+                engine.Evaluate("trCertainUserPro < -left_join(trCertainUser, products, by = 'product')");
+                engine.Evaluate("trCertainUserPro < -trCertainUserPro %>% dplyr::arrange(-time)");
+
+                engine.Evaluate("if (nrow(trCertainUserPro) > 5) { trCertainUserPro =trCertainUserPro[1:5,]}");
+                //достаем 5 последних товаров
+                var trCertainUserPro = engine.GetSymbol("trCertainUserPro").AsVector();
+
+                engine.Evaluate("recData < -left_join(transactions, dplyr::rename(dplyr::select(users, id, wallet), 'from' = 'wallet'))");
+                engine.Evaluate("recData < -dplyr::select(recData, id, value, product)");
+                engine.Evaluate("recData <- recData %>% group_by(id, product) %>% summarise_all(sum)");
+                engine.Evaluate("recData < -spread(recData, key = product, value = value)");
+                engine.Evaluate("recData <- as.data.frame(recData)");
+                engine.Evaluate("rownames(recData) = recData$id");
+                engine.Evaluate("recData = dplyr::select(recData, -id)");
+                engine.Evaluate("recData = as.matrix(recData)");
+
+                engine.Evaluate("r = as(recData, 'realRatingMatrix')");
+                engine.Evaluate("r < -binarize(r, minRating = 2)");
+                engine.Evaluate("recc_model < -Recommender(data = r, method = 'IBCF', parameter = list(k = 30))");
+                engine.Evaluate("recc_predicted < -predict(object = recc_model, newdata = r[2,])");
+
+                engine.Evaluate("recc_predicted @itemLabels[recc_predicted@items[[1]]]");
+
+                engine.Evaluate("   products$product < - as.character(products$product)");
+                engine.Evaluate("indexTitle = match(products$product, pred)");
+                engine.Evaluate("indexTitle < -na.omit(indexTitle)");
+                engine.Evaluate("pred_data=products$name[indexTitle]");
+
+
+                // вытяиваем стринг вектор с предсказанием
+                var predicted = engine.GetSymbol("pred_data").AsVector();
+                
+            }
+            else if (usertype == "worker")
+            {
+                // взять из бд
+                var userid_fromhtml = "34";
+                engine.Evaluate("users < -sqlQuery(myconn, 'SELECT * FROM users1'");
+                engine.Evaluate("transactions < -sqlQuery(myconn, 'SELECT * FROM transactions2')");
+                engine.Evaluate(" products < -sqlQuery(myconn, 'SELECT * FROM products1')");
+
+                engine.Evaluate("");
+                engine.Evaluate("");
+            }
+            else if (usertype == "employer")
+            {
+                // взять из бд
+                var userid_fromhtml = "34";
+                engine.Evaluate("users < -sqlQuery(myconn, 'SELECT * FROM users1'");
+                engine.Evaluate("transactions < -sqlQuery(myconn, 'SELECT * FROM transactions2')");
+                engine.Evaluate(" products < -sqlQuery(myconn, 'SELECT * FROM products1')");
+
+                engine.Evaluate("");
+                engine.Evaluate("");
+            }
+
+
+        }
+    
 
         private void CurrencyLoad()
         {
---
+++ ChainReactionBack/PrivateProfile.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="PrivateProfile.aspx.cs" Inherits="ChainReactionBack.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
